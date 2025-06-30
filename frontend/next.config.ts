452a932696fe8ed2c989b0a322110e7c9595f5f5
Puck Wang <s9801077@gmail.com>
2025-06-30 23:59:50 +0800

    調整環境變數預設值

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     /* config options here */
     env: {
-        NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5000/api',
+        NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5000',
     },
     output: 'standalone',
 };
